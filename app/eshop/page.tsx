dcb0b7b376b9055b230d85bec89779353ccdc3c8
LACOUR Alexandre <lacour_alexandre@proton.me>
2024-03-21 16:12:23 +0100

    adding cart / multiple items to my shop

--- app/eshop/page.tsx
+++ app/eshop/page.tsx
@@ -1,14 +1,58 @@
 import { BuyButton} from './buyButton'
 
+
+const products: Product[] = [
+    {
+        id: "1",
+        name: "item 1",
+        price: 10,
+        description: "item 1 desc",
+        image: "https://via.placeholder.com/150",
+        url: "https://google.com",
+        quantity: 0
+    },
+    {
+        id: "2",
+        name: "item 2",
+        price: 20,
+        description: "item 2 desc",
+        image: "https://via.placeholder.com/150",
+        url: "https://google.com",
+        quantity: 0
+    },
+    {
+        id: "3",
+        name: "item 3",
+        price: 10,
+        description: "item 3 desc",
+        image: "https://via.placeholder.com/150",
+        url: "https://google.com",
+        quantity: 0
+    }
+]
 export default function Page() {
     return (
 
         <main>
             <div className="flex mt-[20vh] justify-center h-screen">
-            <div className="mx-auto">
-                <h1>abo récurent</h1>
-                <BuyButton />
-            </div>
+                <div className="mx-auto">
+                    <h2>abo récurent</h2>
+                    <BuyButton />
+                </div>
+                <div>
+                    <h2>Produits</h2>
+                    <div className="grid grid-cols-3 gap-4">
+                        {products.map((product) => (
+                            <div key={product.id} className="border p-4">
+                                <h3>{product.name}</h3>
+                                <p>{product.description}</p>
+                                <img src={product.image} alt={product.name} />
+                                <p>{product.price} €</p>
+                                <button className="bg-blue-500 text-white p-2 rounded" onClick={() => console.log('add to cart')}>Ajouter au panier</button>
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
         </main>
     )
