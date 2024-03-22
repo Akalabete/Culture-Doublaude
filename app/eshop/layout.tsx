ddd3b5c90aeec5b1baf4fe2e34c08b71ddd0ea21
LACOUR Alexandre <lacour_alexandre@proton.me>
2024-03-22 11:52:35 +0100

    added a layout to put context wrapper in eshop page

---
+++ app/eshop/layout.tsx
@@ -0,0 +1,13 @@
+
+import { CartProvider } from "../components/CartContext";
+export default function Layout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    
+    return 
+    <CartProvider>
+        <div>{children}</div>
+    </CartProvider>
+}
