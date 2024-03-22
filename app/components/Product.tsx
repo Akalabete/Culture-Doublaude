9952a85cb73c81c6fba71469dba17e79e37e05cc
LACOUR Alexandre <lacour_alexandre@proton.me>
2024-03-22 14:31:20 +0100

    added a swapping color / content to checked / unchecked adding to cart button

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -1,18 +1,30 @@
 'use client';
 
 import React from 'react';
+import { useCart } from './CartContext';
+
+
 interface ProductProps {
     product: Product;
 }
   
 
 const Product: React.FC<ProductProps> = ({product}) => {
+  const { addToCart, cart } = useCart();
+  const isProductInCart = cart.some((item) => item.id === product.id)
   return (
     <div className="border rounded-lg p-4 shadow-md">
         <h3 className="text-lg font-semibold">{product.name}</h3>
         <p className="text-blue-400">{product.price.toFixed(2)} €</p>
-        <button onClick={() => {}} disabled={false} className='mt-2 px-4 py-2 rounded focus-outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'>
-          {true ? 'Ajouter au panier' : 'Produit ajouté'}
+        <button 
+          onClick={() => addToCart(product)} 
+          disabled= {isProductInCart} 
+          className={`mt-2 px-4 py-2 
+          ${isProductInCart?
+            "bg-gray-400 text-gray-600 cursor-not-allowed"
+          : "bg-blue-500 text-white hover:bg-blue-600" } 
+          rounded focus-outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2`}>
+          {isProductInCart ? 'Ajouter au panier' : 'Produit ajouté'}
         </button>
     </div>
   )
